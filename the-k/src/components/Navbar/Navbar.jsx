3c0f290436e98e00495942ae61db464735d91b09
Mathieu JOLY <m.joly@live.fr>
2021-02-16 21:44:32 +0100

    add logo

--- the-k/src/components/Navbar/Navbar.jsx
+++ the-k/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
+import logo from 'img/the_k_logo.svg';
 
 const Navbar = () => {
 
   return (
     <nav className="Navbar">
         <Link to="/">
-          <p>Home</p>
+          <img src={logo} alt='the k logo' />
         </Link>
         <Link to="/register">
           <p>Register</p>
