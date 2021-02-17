05437d86c436cde849dbb9e86939de3a98f98236
Mathieu JOLY <m.joly@live.fr>
2021-02-17 20:59:23 +0100

    add messages

--- the-k/src/pages/Home/Home.jsx
+++ the-k/src/pages/Home/Home.jsx
@@ -1,11 +1,35 @@
-const Home = () => {
+import React, { useState, useEffect } from "react";
+import Cookies from 'js-cookie'
+import jwt_decode from "jwt-decode";
+import './Home.scss';
 
-  return (
-    <nav className="Home">
-    <p>Welcome on My Social Network. This website is a training to Redux and React. We use auth and routing to create a small social media website.</p>
-        <p>hello from public home</p>
-    </nav>
-  );
+const Home = () => {
+  	const [displayError, setDisplayError] = useState('');
+  	const [messages, setMessages] = useState('');
+
+  	const loadPosts = data => {
+  		fetch('http://localhost:1337/posts', {
+  		  method: 'get',
+  		  headers: {
+    		'Content-Type': 'application/json'
+  		  }
+		})
+		.then((response) => response.json())
+		.then((response) => setMessages(response))
+		.catch((error) => setDisplayError('Pas authentifié'));
+  	}
+
+  	useEffect(() => {
+    loadPosts();
+  	}, [])
+
+	return (
+		<nav className="Home">
+    		<p>Welcome on My Social Network. This website is a training to Redux and React. We use auth and routing to create a small social media website.</p>
+        	<p>hello from public home</p>
+        	<p>{displayError}</p>
+    	</nav>
+	);
 };
 
 export default Home;
