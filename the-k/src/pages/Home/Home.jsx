0a6ca42817e269d4571bad8cd8b3f2b187df9490
Mathieu JOLY <m.joly@live.fr>
2021-02-17 18:38:15 +0100

    update homepage

--- the-k/src/pages/Home/Home.jsx
+++ the-k/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ const Home = () => {
 
   return (
     <nav className="Home">
+    <p>Welcome on My Social Network. This website is a training to Redux and React. We use auth and routing to create a small social media website.</p>
         <p>hello from public home</p>
     </nav>
   );
