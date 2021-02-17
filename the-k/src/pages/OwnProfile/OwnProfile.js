821dc80ff6b0ac8daef1cb002c93e7eb749b0e9f
Mathieu JOLY <m.joly@live.fr>
2021-02-17 16:21:12 +0100

    add loadProfile

--- the-k/src/pages/OwnProfile/OwnProfile.js
+++ the-k/src/pages/OwnProfile/OwnProfile.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 import { useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import './OwnProfile.scss';
@@ -6,14 +7,37 @@ import './OwnProfile.scss';
 const OwnProfile = () => {
   const hasID = useSelector(state => state);
   const [displayError, setDisplayError] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
   const { register, handleSubmit, watch, errors } = useForm();
+
+  const loadProfile = () => {
+  		fetch(`http://localhost:1337/users/${hasID}`, {
+  		  method: 'get',
+  		  headers: {
+    		'Authorization': `Bearer ${Cookies.get('token')}`,
+    		'Content-Type': 'application/json'
+  		  }
+		})
+		.then((response) => response.json())
+		.then((response) => {
+			console.log(response)
+			setName(response.username)
+			setDescription(response.description)
+		})
+		.catch((error) => console.log(error));
+  	}
+
+  	useEffect(() => {
+    loadProfile();
+  	}, [])
 
   return (
     <nav className="OwnProfile">
         <p>hello from my own profile again</p>
         <form className="OwnProfile__details">
-		  <input name="username" type="text" placeholder="username" ref={register({ required: true })} />
-		  <input name="description" type="text" placeholder="description" ref={register({ required: true })} />
+		  <input name="username" type="text" placeholder="username" value={name} ref={register({ required: true })} />
+		  <input name="description" type="text" placeholder="description" value={description} ref={register({ required: true })} />
 	  	  <input type="submit" />
 	  	  <p>error: {displayError}</p>
 	    </form>
