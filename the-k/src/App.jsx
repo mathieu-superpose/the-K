612b335047785f500c9294b5493025013ea391e3
Mathieu JOLY <m.joly@live.fr>
2021-02-17 14:58:08 +0100

    add private route

--- the-k/src/App.jsx
+++ the-k/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import 'index.scss';
 
 import Navbar from 'components/Navbar/Navbar';
@@ -9,25 +10,35 @@ import Register from 'pages/Register/Register';
 import Login from 'pages/Login/Login';
 import Profile from 'pages/Profile/Profile';
 
+
+
 const App = () => {
+  const hasID = useSelector(state => state);
+
+  const checkAuth = () => {
+    return hasID==='' ? false :  true
+  }
+
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={props => (
+      checkAuth() ? (
+        <Component {...props} />
+      ) : (
+          <Redirect to={{ pathname: '/login' }} />
+        )
+    )} />
+  )
+
   return (
     <div className="App">
       <Router>
         <Navbar />
         <main>
           <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/profile">
-              <Profile />
-            </Route>
+            <Route path="/" exact component={Home} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <PrivateRoute path="/profile" component={Profile} />
           </Switch>
         </main>
         <Footer />
